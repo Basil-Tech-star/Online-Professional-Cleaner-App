96d2286a7a57029c9dfa9c2d0d2f85db36fe4f7c
Basil Wabuke <babazwabuke4@gmail.com>
2025-02-04 01:04:36 +0300

    Final touches

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../Navbar.css';  // Make sure to import the CSS file
 
 const Navbar = () => {
   return (
     <nav>
-      <ul>
+      <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/signup">Sign Up</Link></li>
         <li><Link to="/login">Login</Link></li>
--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import '../Homepage.css'; // Add this line to import the CSS file for styling
 
 const HomePage = () => {
-  return <h1>Welcome to the Professional Cleaners App!</h1>;
+  return (
+    <div className="home-container">
+      <h1 className="title">Welcome to the Professional Cleaners App!</h1>
+      <div className="services">
+        <h2>Our Services</h2>
+        <ul>
+          <li>Residential Cleaning</li>
+          <li>Office Cleaning</li>
+          <li>Deep Cleaning</li>
+          <li>Move-In/Move-Out Cleaning</li>
+          <li>Carpet and Upholstery Cleaning</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
