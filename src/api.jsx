d52879e038655017295e1a0f592ddf13732a77ab
Basil Wabuke <babazwabuke4@gmail.com>
2025-02-04 13:15:43 +0300

    adding changes to api file

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL =  process.env.REACT_APP_API_URL;  // Adjust the URL to match the backend's URL
+const API_URL =  process.env.REACT_APP_API_URL || 'https://cleaner-app-back-end.onrender.com';  // Adjust the URL to match the backend's URL
 
 export const api = axios.create({
   baseURL: API_URL,
