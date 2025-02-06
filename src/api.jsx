0c7cf4d3087353a4f0adf5bbe67cf06243389a5e
Basil Wabuke <babazwabuke4@gmail.com>
2025-02-06 23:38:42 +0300

    chnages to url

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL =  process.env.REACT_APP_API_URL || 'https://cleaner-app-back-end.onrender.com';  // Adjust the URL to match the backend's URL
+const API_URL =  'https://cleaner-app-back-end.onrender.com';  // Adjust the URL to match the backend's URL
 // const API_URL = 'http://127.0.0.1:5000'
 export const api = axios.create({
   baseURL: API_URL,
