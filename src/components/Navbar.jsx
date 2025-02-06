86d71b8027dbf7189668fdd24994d050f61cebd3
Basil Wabuke <babazwabuke4@gmail.com>
2025-02-07 00:21:26 +0300

    changes to api.jsx file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../Navbar.css';  // Make sure to import the CSS file
+import '../Navbar.css';  
 
 const Navbar = () => {
   return (
